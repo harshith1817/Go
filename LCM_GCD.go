b284748f565dfce047f1d7ac42ad64e99fa676ba
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2024-12-18 18:45:33 +0530

    Update LCM_GCD.go

--- LCM_GCD.go
+++ LCM_GCD.go
@@ -1,48 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func lcm(t1 int, t2 int) {
-    var lcmnum int = 1
-    if t1 > t2 {
-        lcmnum = t1
-    } else {
-        lcmnum = t2
-    }
-    for {
-        if lcmnum % t1 == 0 && lcmnum % t2 == 0 {
-            fmt.Printf("LCM of %d and %d is %d", t1, t2, lcmnum)
-            break
-        }
-        lcmnum++
-    }
-    return
+func lcm(t1, t2 int) int {
+	greater := t1
+	if t2 > t1 {
+		greater = t2
+	}
+	for greater%t1 != 0 || greater%t2 != 0 {
+		greater++
+	}
+	return greater
 }
 
-func gcd(t1 int, t2 int) {
-    var gcdnum int
-    for i: = 1;
-    i <= t1 && i <= t2;
-    i++{
-        if t1 % i == 0 && gcdnum % i == 0 {
-            gcdnum = i
-        }
-    }
-    fmt.Print("gcd is:", gcdnum)
-    return
+func gcd(t1, t2 int) int {
+	for t2 != 0 {
+		t1, t2 = t2, t1%t2
+	}
+	return t1
 }
 
 func main() {
-    var n1, n2, action int
-    fmt.Println("enter 2 positive integers:")
-    fmt.Scanln( & n1)
-    fmt.Scanln( & n2)
-    fmt.Println("enter 1 for lcm and 2 for gcd")
-    fmt.Scanln( & action)
-    switch action {
-        case 1:
-            lcm(n1, n2)
-        case 2:
-            gcd(n1, n2)
-    }
+	var n1, n2, action int
+	fmt.Println("Enter two positive integers:")
+	fmt.Scanln(&n1, &n2)
+	fmt.Println("Enter 1 for LCM or 2 for GCD:")
+	fmt.Scanln(&action)
+
+	switch action {
+	case 1:
+		fmt.Printf("LCM of %d and %d is %d\n", n1, n2, lcm(n1, n2))
+	case 2:
+		fmt.Printf("GCD of %d and %d is %d\n", n1, n2, gcd(n1, n2))
+	default:
+		fmt.Println("Invalid choice")
+	}
 }
